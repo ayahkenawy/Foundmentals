b68165228af6fcfce5d01a655ff1c9a960c04aef
Aya Ahmed Kenawy <ayahkenawy4490@gmail.com>
2021-09-09 22:11:40 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,19 @@
+import keyword
+print(keyword.kwlist)
+#Challange
+user_input = input("What's my fav food ?")
+if user_input =="Seafood":
+    print("You are right !")
+else:
+    print("No")
+print("Thanks for playing")
+#functions
+def say_hello() :
+    print("Hello!")
+say_hello()
+#callange Cities
+def fav_city(name):
+    print("My fav city is",name)
+fav_city("Portsaid")
+fav_city("Dahab")
+fav_city("Alex")
